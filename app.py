efccf6fc93a18c3e63de08acbe20bcd7fc3744e1
Vinay Vidyasagar <86476092+TheLazyIndianTechie@users.noreply.github.com>
2023-07-05 09:25:53 +0530

    Added flask and jinja templates for a simple webapp. Work in progress

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
 #Import OpenAI
 from langchain.llms import OpenAI
+from flask import Flask, request, render_template
+
 
 #Set llm temperature
 llm = OpenAI(temperature=0.9)
 
+
+
+#Webapp code
+app = Flask(__name__)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+#jinja code
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    # Process the form data here
+    return f'Hello, {name}!'
+
+if __name__ == '__main__':
+    app.run()
+### End Webapp Code
+
+
+
+greeting = "Hello, there!"
 
 name = input("Enter your name: ")
 print("Hello, " + name)
